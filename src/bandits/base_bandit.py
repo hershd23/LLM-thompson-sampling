626042526a7a5f265af4e28b36e838eb19c7883f
hershd23 <hershdhillon23@gmail.com>
2024-02-16 01:33:01 -0500

    Added the agent class

--- src/bandits/base_bandit.py
+++ src/bandits/base_bandit.py
@@ -1,15 +1,19 @@
 import numpy as np
 
+def random_argmax(vector):
+  """Helper function to select argmax at random... not just first one."""
+  index = np.random.choice(np.where(vector == vector.max())[0])
+  return index
+
 class BaseBandit(object):
     """
     Base class for all the bandit algorithms
     """
-
     def __init__(self):
         """Initializes the bandit algorithm"""
         raise NotImplementedError
 
-    def update_observations(self, arm, reward):
+    def update_observation(self, arm, reward):
         """Updates the observation for the bandit algorithm"""
         raise NotImplementedError
     
